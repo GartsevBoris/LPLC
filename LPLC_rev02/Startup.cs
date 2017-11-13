3b5b548768acb118d956ec758864c239a2e33cf3
GartsevBoris <garcevboris@gmail.com>
2017-11-13 21:14:24 +0200

    Add project files.

---
+++ LPLC_rev02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LPLC_rev02.Startup))]
+namespace LPLC_rev02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
